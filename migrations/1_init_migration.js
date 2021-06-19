f05c9b76c3c5a00904c3dc5e24bca8d8fac0ab83
dream <dream@gmail.com>
2021-06-19 21:21:41 +0700

    init migrate

---
+++ migrations/1_init_migration.js
@@ -0,0 +1,6 @@
+const Migrations = artifacts.require("Migrations");
+// 1_initial_migration.js
+const Migrations = artifacts.require("Migrations");
+module.exports = function (deployer) {
+     deployer.deploy(Migrations);
+};
